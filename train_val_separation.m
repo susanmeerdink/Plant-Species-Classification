04b596a80b99034553e259c50384ecd21247f084
Susan Meerdink <susan.meerdink@gmail.com>
2017-08-04 16:39:21 -0700

    adding in flexibility of percent of dataset used for training

--- train_val_separation.m
+++ train_val_separation.m
@@ -2,7 +2,6 @@
 % Susan Meerdink
 % 7/30/17
 
-function [name_train, name_valid] = train_val_separation(metadata, iterations)
 
 name_train = cell(iterations,0);
 name_valid = cell(iterations,0);
@@ -15,7 +14,6 @@ for d = 1:size(dominantList,1)
     dominant_indices = strmatch(dominantList(d), metadata(:,15));
     pix_in_poly = metadata(dominant_indices,:); % Get all pixels that belong to this dominant species
     name_of_poly = unique(pix_in_poly(:,3));  % Get the number of polygons for dominant species
-    num_train_poly = round(0.5 * size(name_of_poly, 1));  % Determine how many polygons are going to be set aside for training
     for t = 1:iterations
         idx_valid = (1:size(name_of_poly,1))';
         [name_train_poly(t,:), idx] = datasample(name_of_poly, num_train_poly,'Replace',false);
